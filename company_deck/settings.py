8e064f54c2c3ba7e595abde4735638f3fc4d1fe9
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-25 22:37:50 +0100

    dosc: company project

--- company_deck/settings.py
+++ company_deck/settings.py
@@ -163,7 +163,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'adeoluwafisayomi@gmail.com'
-EMAIL_HOST_PASSWORD = '3949oreoluwa'
+
 
 
 MEDIA_URL = '/images/'
